955190c36f7052ce0f125321d9ca9f66ccc7bcd0
Jack Jefferson <eptgen@gmail.com>
2018-06-14 02:17:57 -0700

    tried to multithread (DOESN'T WORK)

--- choleskytest.cpp
+++ choleskytest.cpp
@@ -2,11 +2,39 @@
 #include <random>
 #include <time.h>
 #include <Eigen/Dense>
+#include <vector>
+#include <pthread.h>
 #include "unittests.h"
 #include "choleskytest.h"
 
 using namespace std;
 using namespace Eigen;
+
+struct gemm_args
+{
+	MatrixXd* A;
+	int i;
+	int j;
+	int k;
+	int b;
+	bool* complete;
+};
+
+void *gemm(void *arguments)
+{
+	gemm_args *args = (gemm_args*) arguments;
+	MatrixXd *A = (MatrixXd*) args->A;
+	int i = (long) args->i;
+	int j = (long) args->j;
+	int k = (long) args->k;
+	int b = (long) args->b;
+	bool *complete = args->complete;
+	// cout << "A" << i << j << " -= A" << i << k << " * A" << j << k << "^T" << endl;
+	A->block(i * b, j * b, b, b) -= A->block(i * b, k * b, b, b) * A->block(j * b, k * b, b, b).transpose();
+	// cout << "setting complete to true" << endl;
+	complete = new bool(true);
+	pthread_exit(NULL);
+}
 
 void cholesky(MatrixXd& A, int p, int b)
 {
@@ -21,11 +49,73 @@ void cholesky(MatrixXd& A, int p, int b)
 			A.block(i * b, k * b, b, b) = A.block(k * b, k * b, b, b).transpose().triangularView<Upper>().solve<OnTheRight>(A.block(i * b, k * b, b, b));
 			// A.block(i * b, k * b, b, b) = A.block(i * b, k * b, b, b) * A.block(k * b, k * b, b, b).transpose().inverse();
 		}
+		vector<pthread_t> threads;
+		// threads.reserve(p * p);
+		vector<bool*> completes;
+		// completes.reserve(p * p);
 		for (int i = k + 1; i < p; i++)
 		{
 			for (int j = k + 1; j <= i; j++)
 			{
-				A.block(i * b, j * b, b, b) -= A.block(i * b, k * b, b, b) * A.block(j * b, k * b, b, b).transpose();
+				// cout << "i=" << i << ", j=" << j << endl;
+				bool *f = new bool(false);
+				// cout << "bool *f = new bool(false);" << endl;
+				completes.push_back(f);
+				// cout << "completes.push_back(f);" << endl;
+				pthread_t thread;
+				// cout << "pthread_t thread;" << endl;
+				// cout << "threads.size() == " << threads.size() << endl;
+				threads.push_back(thread);
+				// cout << "threads.push_back(thread);" << endl;
+				gemm_args args;
+				// cout << "gemm_args args;" << endl;
+				args.A = &A;
+				// cout << "args.A = &A;" << endl;
+				args.i = i;
+				args.j = j;
+				args.k = k;
+				args.b = b;
+				args.complete = completes[i];
+				// cout << "creating thread" << endl;
+				pthread_create(&threads[i], NULL, gemm, (void*) &args);
+				// cout << "I am done creating the thread " << i << " " << j << " " << k << " " << p << endl;
+			}
+		}
+		// cout << "out of the loop " << k << endl;
+		int start = 0; // to ensure that it's not checking the same thing twice
+		while (true)
+		{
+			bool shouldBreak = true;
+			/*
+			for (int i = k + 1; i < p; i++)
+			{
+				for (int j = k + 1; j <= i; j++)
+				{
+					if (!completes[i * p + j])
+					{
+						start = i;
+						shouldBreak = false;
+						break;
+					}
+				}
+				if (!shouldBreak)
+				{
+					break;
+				}
+			}
+			*/
+			for (int i = start; i < threads.size(); i++)
+			{
+				if (!(*completes[i]))
+				{
+					start = i;
+					shouldBreak = false;
+					break;
+				}
+			}
+			if (shouldBreak)
+			{
+				break;
 			}
 		}
 	}
@@ -64,7 +154,7 @@ int main()
 	cout << "Real Answer: " << endl;
 	cout << answer << endl << endl;
 	
-	cholesky(testMatrix, 4, 4); // 4 * 4 == 16 == size
+	cholesky(testMatrix, 4, 4); // 2 * 2 == 4 == size
 	
 	cout << "Test Answer: " << endl;
 	cout << testMatrix << endl << endl;
